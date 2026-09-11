5d97c514db0a05ed08fb14dd3a9945a9cec694d3
Sevastopol12 <chkalov253@gmail.com>
2026-09-11 16:43:54 +0700

    feat(utils): add hash function

--- data_managment_system/utils/helper/__init__.py
+++ data_managment_system/utils/helper/__init__.py
@@ -1,9 +1,17 @@
 import base64
+from hashlib import sha3_256
 
 
 def encode_content(raw_bytes: bytes) -> str:
-    encoded_bytes = base64.b64encode(raw_bytes).decode("utf-8")
+    encoded_bytes = base64.urlsafe_b64encode(raw_bytes).decode("utf-8")
     return encoded_bytes
 
 
-__all__ = ["encode_content"]
+def hash_content(encoded_bytes: str) -> str:
+    sha_object = sha3_256()
+    sha_object.update(encoded_bytes.encode())
+
+    return sha_object.hexdigest()
+
+
+__all__ = ["encode_content", "hash_content"]
